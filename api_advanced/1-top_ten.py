4a10f66db931d72f226baefc262451728caac545
ateteN <n.atete@alustudent.com>
2025-02-27 02:28:40 -0800

    api_advanced

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python3
-""" Script to get the first 10 hot
-    posts on Reddit
-"""
-from requests import get
+"""Contains top_ten function"""
+import requests
 
 
 def top_ten(subreddit):
-    """get first 10 hot post for a subreddit"""
-    if subreddit and type(subreddit) is str:
-        url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
-        headers = {'user-agent': 'my-app/0.0.1'}
-        params = {'limit': 10}
-        req = get(url, params=params, headers=headers, allow_redirects=False)
-        if req.status_code == 200:
-            data = req.json()
-            posts = data.get('data', {}).get('children', {})
-            for post in posts:
-                print(post.get('data').get('title'))
-        else:
-            print(None)
+    """Print the titles of the 10 hottest posts on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "0x16-api_advanced:project:\
+v1.0.0 (by /u/firdaus_cartoon_jr)"
+    }
+    params = {
+        "limit": 10
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
+        print("None")
+        return
+    results = response.json().get("data")
+    [print(c.get("data").get("title")) for c in results.get("children")]
